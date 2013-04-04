42ad48d1072da70527bf0ab4f3001774b6a82354
Michael Turek <mturek@mit.edu>
2013-04-03 23:39:31 -0400

    Increased min sdk ersion to 11

--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.myfirstapp;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class MainActivity extends Activity implements MyListFragment.OnItemSelectedListener{
 
