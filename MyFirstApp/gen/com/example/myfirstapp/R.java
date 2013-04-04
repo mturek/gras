e16a463547176c013e2925fe08bd64ed228d9c72
yoanag <Yoana@18.111.22.11>
2013-04-03 23:32:07 -0400

    my fragment app

--- MyFirstApp/gen/com/example/myfirstapp/R.java
+++ MyFirstApp/gen/com/example/myfirstapp/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.gras.assignmentgps;
+package com.example.myfirstapp;
 
 public final class R {
     public static final class attr {
@@ -24,19 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int buttonStart=0x7f080006;
-        public static final int buttonStop=0x7f080005;
-        public static final int radioGPS=0x7f080004;
-        public static final int radioGroup1=0x7f080000;
-        public static final int radioNetwork=0x7f080002;
-        public static final int radioNetworkWifi=0x7f080003;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080007;
-        public static final int textViewLocation=0x7f080008;
+        public static final int action_settings=0x7f080004;
+        public static final int button1=0x7f080003;
+        public static final int detailFragment=0x7f080001;
+        public static final int detailsText=0x7f080002;
+        public static final int listFragment=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int details=0x7f030001;
+        public static final int fragment_overview=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -44,18 +41,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int currentLocation=0x7f05000c;
-        public static final int error=0x7f050005;
-        public static final int gps=0x7f050009;
         public static final int hello_world=0x7f050002;
-        public static final int latitude=0x7f050003;
-        public static final int longitude=0x7f050004;
-        public static final int network=0x7f050007;
-        public static final int network_wifi=0x7f050008;
-        public static final int noLocation=0x7f05000d;
-        public static final int selection=0x7f050006;
-        public static final int start=0x7f05000a;
-        public static final int stop=0x7f05000b;
     }
     public static final class style {
         /** 
