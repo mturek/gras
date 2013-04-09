7cfe99aacae6c201d1078151c3866b96c63e2159
yoanag <Yoana@18.111.22.11>
2013-04-09 11:02:54 -0400

    Task Details updated

--- TaskDetails/gen/com/example/taskdetails/R.java
+++ TaskDetails/gen/com/example/taskdetails/R.java
@@ -22,18 +22,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int trash=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int buttonSwap=0x7f080004;
-        public static final int description=0x7f080000;
-        public static final int labelPeoples=0x7f080001;
-        public static final int labelRemainders=0x7f080005;
-        public static final int taskPrototypes=0x7f080003;
-        public static final int taskPrototypesView=0x7f080002;
-        public static final int toggleButton=0x7f080006;
-        public static final int toggleButton2=0x7f080007;
-        public static final int toggleButton3=0x7f080008;
+        public static final int Due=0x7f080002;
+        public static final int action_settings=0x7f08000b;
+        public static final int buttonSwap=0x7f08000a;
+        public static final int description=0x7f080001;
+        public static final int imageView1=0x7f080000;
+        public static final int labelPeoples=0x7f080003;
+        public static final int labelRemainders=0x7f080006;
+        public static final int taskPrototypes=0x7f080005;
+        public static final int taskPrototypesView=0x7f080004;
+        public static final int toggleButton=0x7f080007;
+        public static final int toggleButton2=0x7f080008;
+        public static final int toggleButton3=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,6 +45,7 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Due=0x7f050008;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int buttonSwap=0x7f050003;
