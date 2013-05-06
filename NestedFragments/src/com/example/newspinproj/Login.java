94d121ea8ca77013488378d85e6089cb650dab8b
Michael Turek <mturek@mit.edu>
2013-05-06 03:03:04 -0400

    working login scree

--- NestedFragments/src/com/example/newspinproj/Login.java
+++ NestedFragments/src/com/example/newspinproj/Login.java
@@ -3,8 +3,11 @@ package com.example.newspinproj;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 
 public class Login extends Activity {
 
@@ -23,7 +26,8 @@ public class Login extends Activity {
 	      public void onClick(View view) {
 	        Intent intent = new Intent(Login.this, MainActivity.class);
 	        Bundle bundle = new Bundle();
-	        bundle.putString("uname", "Michael");
+	        String username = ((EditText) findViewById(R.id.username)).getText().toString();
+	        bundle.putString("uname", username);
 	        intent.putExtra("unamestuff", bundle);
 	        startActivity(intent);
 	      }
